529977068fdc17601e5fc83a36894fabb620d309
zza <zza@users.noreply.github.com>
2026-08-19 14:25:23 +0800

    Align PCkey version metadata to 0.5

--- 0.5/include/pckey/version.hpp
+++ 0.5/include/pckey/version.hpp
@@ -5,6 +5,6 @@
 namespace pckey {
 
 inline constexpr std::wstring_view kProductName = L"PCkey";
-inline constexpr std::wstring_view kVersion = L"0.4.4-dev";
+inline constexpr std::wstring_view kVersion = L"0.5.0-dev";
 
 }  // namespace pckey
